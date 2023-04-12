a9578e881149e0adccd187c282baa74339ae0e75
Finnegan Thompson <48135606+FinneganThompson@users.noreply.github.com>
2023-04-11 21:13:11 -0600

    Create RNG.h

--- RNG.h
+++ RNG.h
@@ -1,3 +1,17 @@
 /*
-* Includes all random number generation functionality. 
+* RN.h
+* CSCI 1300 Project 3, Spring 2023
+* August Milliken & Finnegan Thompson
+*
+* File includes random number generating functions
 */
+
+#include <cstdlib>
+
+#ifndef RNG_H
+#define RNG_H
+
+bool actionOccurs(int probabilityOfAction); // returns wether or not an action occurs based off of its probability
+int randIntBetweenOneAndTen(); // Returns random int between one and ten (not including 0, including 10)
+
+#endif
