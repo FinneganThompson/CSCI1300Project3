eaa79f3c28439841e5dd4f411d0301bc57a0e52b
Finnegan Thompson <48135606+FinneganThompson@users.noreply.github.com>
2023-04-12 18:07:36 -0600

    Update RNG.cpp

--- RNG.cpp
+++ RNG.cpp
@@ -1,1 +1,23 @@
+/*
+* RNG.cpp
+* CSCI 1300 Project 3, Spring 2023
+* August Milliken & Finnegan Thompson
+*
+* File includes random number generating functions
+*/
+
+
+#include "RNG.h"
+
+
+// Accepts a probability as a percentage (0 to 100) and uses a random dice roll to get an outcome within or out of that probability range. 
+bool actionOccurs(int probabilityOfAction)
+{
+    return false; // Placeholder return
+}
+
+int randIntBetweenOneAndTen() // Returns random int between one and ten (not including 0, including 10)
+{
+
+}
 
