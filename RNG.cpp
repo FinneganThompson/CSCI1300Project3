e58927b87eed8ead59d947c841aed1eb452d034d
Finnegan Thompson <48135606+FinneganThompson@users.noreply.github.com>
2023-04-16 17:34:48 -0600

    Update RNG.cpp

--- RNG.cpp
+++ RNG.cpp
@@ -8,10 +8,11 @@
 
 
 #include "RNG.h"
- 
+
 // Accepts a probability as a percentage (0 to 100) and uses a random dice roll to get an outcome within or out of that probability range. 
 bool doesActionOccur(int probabilityOfAction)
 {
+    srand(time(0)); // Set our seed with the system time
     int randomInt = rand() % 100; // Take the last 2 digits of the random int
     
     if (randomInt < probabilityOfAction) // This gives us the correct probabilties
@@ -23,6 +24,7 @@ bool doesActionOccur(int probabilityOfAction)
 
 int randIntBetweenOneAndTen() // Returns random int between one and ten (not including 0, including 10)
 {   
+    srand(time(0)); // Set our random num seed
     int randomInt = rand() % 10 +1 ; // Gets random number between one and 10
     return randomInt;
 }
