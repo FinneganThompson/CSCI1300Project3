93a082a01eb3285a6def94036e4e37e72fbeacb9
Finnegan Thompson <48135606+FinneganThompson@users.noreply.github.com>
2023-04-16 17:42:15 -0600

    Update RNG.h

--- RNG.h
+++ RNG.h
@@ -16,5 +16,6 @@ using namespace std;
 
 bool doesActionOccur(int probabilityOfAction); // returns wether or not an action occurs based off of its probability
 int randIntBetweenOneAndTen(); // Returns random int between one and ten (not including 0, including 10)
+int randIntOnRange(int lowerBound, int upperBound); // Both bounds are included. Returns -1 for failure
 
 #endif
