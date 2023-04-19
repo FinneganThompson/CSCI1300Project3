7fdbcebaad13b971d8495df4242d11f8b51eac42
AugustMill <123115700+AugustMill@users.noreply.github.com>
2023-04-19 17:48:57 -0600

    Update mainDriver.cpp

--- mainDriver.cpp
+++ mainDriver.cpp
@@ -54,44 +54,94 @@ for room spaces
 // to run: g++ mainDriver.cpp item.cpp Monster.cpp Sorcerer.cpp party.cpp Map.cpp
 
 /*
-+-------------+
-| INVENTORY   |
-+-------------+
-| Gold        | 100
-| Ingredients | 0 kg
-| Cookware    | P: 0 | F: 0 | C: 0
-| Weapons     | C: 0 | S: 0 | R: 0 | B: 0 | L: 0
-| Armor       | 0
-| Treasures   | R: 0 | N: 0 | B: 0 | C: 0 | G: 0
+Algorithm prints out everything that is in the party's inventory 
+Accepts the party's inventory object 
+Create vectors for the cookware, weapons, and treasure, of their respective types
+Create ints to hold the amount of each type of cookware, weapons, and treasure respectively 
+Loop through each vector and count the number of each type for cookware, weapons, and treasure respectively
 */
 
-
-void printInventory(party mainParty){
-    inventory partyInventory = mainParty.partyInventory_;
-    vector<cookware> partyCookware = partyInventory.cookwareAailible();
-    // could be useful to make this a separtate function but yeah
-    int pots;
-    int fryingPans;
-    int cauldrons;
+void printInventory(inventory partyInventory){
+    // creates a vector to hold the cookware in the party's inventory
+    vector<cookware> partyCookware = partyInventory.cookwareAvailible();
+    // creates ints to hold the number of each individual type of cookware
+    int pots = 0, fryingPans = 0, cauldrons = 0;
+    // loops through the vector to count the number of each type of cookware
     for(int i = 0; i < partyCookware.size(); i++){
-        if(partyCookware[i] == 'P'){
-            
+        if(partyCookware.at(i).getType() == 'P'){
+            pots++;
+        }
+        else if(partyCookware.at(i).getType() == 'F'){
+            fryingPans++;
+        }
+        else{
+            cauldrons++;
         }
     }
+    // creates a vector to hold the weapons in the party's inventory
+    vector<weapon> partyWeapons = partyInventory.weaponsAvalible();
+    // creates ints to hold the number of each individual type of weapon
+    int clubs = 0, spears = 0,rapiers = 0, batAxes = 0, longSwords = 0;
+    // loops through the vector to count the number of each type of weapon
+    for(int i = 0; i < partyWeapons.size(); i++){
+        if(partyWeapons.at(i).type = 'C'){
+            clubs++;
+        }
+        else if(partyWeapons.at(i).type == 'S'){
+            spears++;
+        }
+        else if(partyWeapons.at(i).type == 'R'){
+            rapiers++;
+        }
+        else if(partyWeapons.at(i).type == 'B'){
+            batAxes++;
+        }
+        else{
+            longSwords++;
+        }
+    }
+    // creates a vector to hold the treasure in the party's inventory
+    vector<treasure> partyTreasure = partyInventory.treasureAvailible();
+    // creates ints to hold the number of each individual type of treasure
+    int rings = 0, necklaces = 0, bracelets = 0, circlets = 0, goblets = 0;
+    // loops through the vector to count the number of each type of treasure
+    for(int i = 0; i < partyTreasure.size(); i++){
+        if(partyTreasure.at(i).type == 'R'){
+            rings++;
+        }
+        else if(partyTreasure.at(i).type == 'N'){
+            necklaces++;
+        }
+        else if(partyTreasure.at(i).type == 'B'){
+            bracelets++;
+        }
+        else if(partyTreasure.at(i).type == 'C'){
+            circlets++;
+        }
+        else{
+            goblets++;
+        }
+    }
+    // prints out the whole inventory, including the counts for each item in the inventory
     cout << "+-------------+\n| INVENTORY   |\n+-------------+" << endl <<
     "| Gold        | " << partyInventory.goldAvalible() << endl << 
-    "| Ingredients | " << partyInventory.totalIngredientsAvliable() << endl << 
-    "| Cookware    | " << partyCookware.
-    
+    "| Ingredients | " << partyInventory.totalIngredientsAvliable() << " kg" << endl << 
+    "| Cookware    | P: " << pots << " | F: " << fryingPans << " | C: " << cauldrons << endl <<
+    "| Weapons     | C: " << clubs << " | S: " << spears << " | R: " << rapiers << " | B: " << batAxes << " | L: " << longSwords << endl <<
+    "| Armor       | " << partyInventory.armorAvalible() << endl << 
+    "| Treasures   | R: " << rings << " | N: " << necklaces << " | B: " << bracelets << " | C: " << circlets << " | G: " << goblets << 
+    endl << endl;
 }
 
+/*
+takes party so that it can be passed to printInventory
+*/
 
-
-void merchantMenu()
+void merchantMenu(party mainParty)
 {
     cout << "If you're looking to get supplies, you've come to the right place." << endl <<
-    "I would be happy to part with some of my wares...for the proper price!" << endl;
-    //call inventory function
+    "I would be happy to part with some of my wares...for the proper price!\n" << endl;
+    printInventory(mainParty.partyInventory_);
     int menuChoice = 0;
     cin >> menuChoice;
     while(menuChoice != 6){
@@ -153,15 +203,14 @@ void phase1(){
     mainParty.addPlayer(member4);
     mainParty.partyInventory_.addGold(100);
     Sorcerer s("Sorcerer", 6);
-    cout << "Between the 5 of you, you have 100 gold pieces.\nYou will need to spend the some of your money on the following items:" << endl;
+    cout << "Between the 5 of you, you have 100 gold pieces.\nYou will need to spend the some of your money on the following items:\n" << endl;
     cout << "- INGREDIENTS. To make food, you have to cook raw ingredients." << endl <<
     "- COOKWARE. If you want to cook, you have to have cookware first." << endl << 
     "- WEAPONS. You'll want at least one weapon per party member to fend off monsters." << endl << 
-    "- ARMOR. Armor increases the chances of surviving a monster attack." << endl <<
+    "- ARMOR. Armor increases the chances of surviving a monster attack.\n" << endl <<
     "You can spend all of your money here before you start your journey, or you can save some to spend on merchants along the way." << endl << 
-    "But beware, some of the merchants in this dungeon are shady characters, and they won't always give you a fair price..." << endl;
-    
-    //merchant menu
+    "But beware, some of the merchants in this dungeon are shady characters, and they won't always give you a fair price...\n" << endl;
+    merchantMenu(mainParty);
     /*vector<player> players = mainParty.getPlayers();
     for(int i = 0; i < 5; i++){
         cout << players.at(i).isUserPlayer << endl;
