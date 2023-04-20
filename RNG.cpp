a16715a10bafe0fb4fe55d46ebf7bde5c77e5902
Finnegan Thompson <48135606+FinneganThompson@users.noreply.github.com>
2023-04-20 14:46:51 -0600

    Make rng a class

--- RNG.cpp
+++ RNG.cpp
@@ -9,11 +9,14 @@
 
 #include "RNG.h"
 
+RNG::RNG()
+{
+    srand(time(NULL)); // Call our srand function (applies globally)
+}
 
 // Accepts a probability as a percentage (0 to 100) and uses a random dice roll to get an outcome within or out of that probability range. 
-bool doesActionOccur(int probabilityOfAction)
+bool RNG::doesActionOccur(int probabilityOfAction)
 {
-    srand(time(0)); // Set our seed with the system time
     int randomInt = rand() % 100; // Take the last 2 digits of the random int
     
     if (randomInt < probabilityOfAction) // This gives us the correct probabilties
@@ -23,16 +26,14 @@ bool doesActionOccur(int probabilityOfAction)
     else return false; 
 }
 
-int randIntBetweenOneAndTen() // Returns random int between one and ten (not including 0, including 10)
+int RNG::randIntBetweenOneAndTen() // Returns random int between one and ten (not including 0, including 10)
 {   
-    srand(time(0)); // Set our random num seed
     int randomInt = rand() % 10 +1 ; // Gets random number between one and 10
     return randomInt;
 }
 
-int randIntOnRange(int lowerBound, int upperBound)
+int RNG::randIntOnRange(int lowerBound, int upperBound)
 {
-    srand(time(0)); // Set random seed
     if (lowerBound >= upperBound)
     {
         return -1;
